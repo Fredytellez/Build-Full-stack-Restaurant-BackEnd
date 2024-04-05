264249c8b400899b0024e7cdf6ddebec0918eaf3
Fredy Pena <tredy@outlook.com>
2024-04-04 19:50:46 -0500

    carpeta controller y models

---
+++ controller/reservation.js
@@ -0,0 +1,23 @@
+import ErrorHandler from "../error/error";
+import { Reservation } from "../models/reservationSchema"; // importa el modelo Reservation de reservationSchema
+
+export const sendReservation = async (req, res, next) => { //  tres argumentos: req (la solicitud), res (la respuesta) y next (la función de middleware siguiente en la cadena).
+  const { firstName, lastName, email, phone, date, time } = req.body; // datos necesarios para el cuerpo de la solicitud 
+  if (!firstName || !lastName || !email || !phone || !date || !time) { // Si alguno de los campos está ausente, devuelve un objeto de error utilizando el objeto
+    return next(new ErrorHandler("Please fill full reservation form!"));
+  }
+  try {
+    await Reservation.create(firstName, lastName, email, phone, date, time); //  Este método crea una instancia y espera que se le pase un objeto con los campos de la reserva como propiedades.
+    res.status(200).json({
+      success: true,
+      message: "Reservation Sent Successfully",
+    });
+  } catch (error) {
+    if (error.name === "ValidationError") {  // usara el ErrorHandler para notificar error
+      const validationErrors = Object.values(error.errors).map(
+        (err) => err.message
+      );
+      return next(new ErrorHandler(validationErrors.join(" , "), 400));
+    }
+  }
+};
---
+++ models/reservationSchema.js
@@ -0,0 +1,40 @@
+// Definir un esquema de Mongoose 
+
+import mongoose from "mongoose"; // para definir el esquema de mongoose
+import validator from "validator"; // importa validador de campos del esquema 
+
+const reservationSchema = new mongoose.Schema({ // Define el  esquema de Mongoose 
+    firstName: { // definición de la estructura y validación de campos 
+        type: String,
+        required: true,
+        minLength: [3, "First name must contain at least 3 characters"],
+        maxLength: [30, "First name cannot exceed 3 characters"],
+    },
+    lastName: {
+        type: String,
+        required: true,
+        minLength: [3, "First name must contain at least 3 characters"],
+        maxLength: [30, "First name cannot exceed 3 characters"],
+    },
+    email: {
+        type: String,
+        required: true,
+        validate: [validator.isEmail, "Provide a valid email"]
+    },
+    phone: {
+        type: String,
+        required: true,
+        minLength: [10, "Phone number must contain only 10 digits" ],
+        maxLength: [10, "Phone number must contain only 10 digits" ]
+    },
+    time: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+        required: true
+    }
+})
+
+export const Reservation = mongoose.model("Reservation", reservationSchema) // Exporta el modelo Reservation, mongoose.model() se utiliza para compilar el esquema reservationSchema
